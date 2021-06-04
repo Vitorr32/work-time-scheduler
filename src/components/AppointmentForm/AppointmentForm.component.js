1ff6b5e5f184cb13670e5960bdedea7a817c5125
Vitor Rodrigues Costa <vitorc1206@gmail.com>
2021-06-04 18:03:03 -0300

    Started implementation of the recurrent event form and process of selection

--- src/components/AppointmentForm/AppointmentForm.component.js
+++ src/components/AppointmentForm/AppointmentForm.component.js
@@ -1,4 +1,4 @@
-import { Input, Form, Modal, InputNumber, DatePicker, Button, Checkbox, Tooltip, Select } from 'antd';
+import { Input, Form, Modal, InputNumber, DatePicker, Button, Checkbox, Tooltip, Select, TimePicker } from 'antd';
 import React from 'react';
 import { connect } from 'react-redux';
 import { addAppointment, addJob } from '../../redux/appointment/appointment.actions';
@@ -8,7 +8,8 @@ import './AppointmentForm.styles.scss';
 import { JOB_NOT_STARTED, SCHEDULE_FREE_TIME, SCHEDULE_FULL, SCHEDULE_WORK_ONLY } from '../../utils/constants';
 import { CoffeeOutlined, FieldTimeOutlined, HourglassOutlined } from '@ant-design/icons';
 import { createPeriodObject, getAllVacatedSpacesInPeriodUntilDueDate, getTotalHoursOfPeriods, mergeContinousAppointmentsInDifferentPeriods, verifyAppointmentDisponibility } from '../../utils/periods';
-import { Option } from 'antd/lib/mentions';
+
+const { RangePicker } = TimePicker;
 
 const layout = {
     labelCol: { span: 8 },
@@ -24,6 +25,7 @@ class AppointmentForm extends React.Component {
         this.state = {
             isModalVisible: false,
             isFutherActionModalVisible: false,
+            isRecurrentEvent: false,
             appointmentPreview: null,
             appointmentPeriods: [],
             appointmentSuccessful: false,
@@ -35,9 +37,11 @@ class AppointmentForm extends React.Component {
         this.setState({
             isModalVisible: false,
             isFutherActionModalVisible: false,
+            isRecurrentEvent: false,
             appointmentPreview: null,
             appointmentPeriods: [],
-            appointmentSuccessful: false
+            appointmentSuccessful: false,
+            showTimeToDueDateInputField: false
         })
     }
 
@@ -69,6 +73,13 @@ class AppointmentForm extends React.Component {
     }
 
     previewPeriods([lastChange], values) {
+        console.log(values);
+        const recurrentEvent = values.find(inputData => inputData.name.includes('recurrentEvent'));
+        //No need to set the appointments preview to an recurrent event
+        if (recurrentEvent.value) {
+            return;
+        }
+
         const hours = values.find(inputData => inputData.name.includes('hours'));
         const continuousPeriod = values.find(inputData => inputData.name.includes('continuousPeriod'));
         const dueDate = values.find(inputData => inputData.name.includes('dueDate'));
@@ -173,6 +184,8 @@ class AppointmentForm extends React.Component {
     render() {
         const enabledForm = this.allowFormToBeSubmitted();
 
+        console.log(this.formRef);
+
         return (
             <React.Fragment>
                 <Button type="primary" onClick={() => this.setState({ isModalVisible: true })}>Add Event</Button>
@@ -215,90 +228,175 @@ class AppointmentForm extends React.Component {
                         </Form.Item>
 
                         <Form.Item
+                            label="Recurrent Event"
+                            name="recurrentEvent"
+                            valuePropName='checked'
+                        >
+                            <Checkbox disabled={!enabledForm} onChange={(event) => this.setState({ isRecurrentEvent: event.target.checked })}></Checkbox>
+                        </Form.Item>
+
+                        <Form.Item
                             label="Description"
                             name="description"
                         >
                             <Input.TextArea disabled={!enabledForm} />
                         </Form.Item>
 
-                        <Form.Item
-                            label="Price"
-                            name="price"
-                        >
-                            <InputNumber
-                                formatter={price => `$ ${price}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-                                parser={price => price.replace(/\$\s?|(,*)/g, '')}
-                                min={0}
-                                precision={2}
-                                disabled={!enabledForm}
-                            />
-                        </Form.Item>
-
-                        <Form.Item
-                            label="Estimated Hours"
-                            name="hours"
-                            rules={[{ required: true, message: 'The ETA is required to allow the scheduler to distribute time' }]}
-                        >
-                            <InputNumber
-                                disabled={!enabledForm}
-                                min={0} />
-                        </Form.Item>
-
-                        <div className="due-date-wrapper">
-                            {
-                                !this.state.showTimeToDueDateInputField
-                                    ?
+                        {
+                            this.state.isRecurrentEvent
+                                ?
+                                <React.Fragment>
                                     <Form.Item
-                                        label="Due Date"
-                                        name="dueDate"
-                                        rules={[{ required: true, message: 'The dude date is required' }, { validator: this.validateDueDate }]}
+                                        label="Time"
+                                        name="recurrentEventTime"
+                                        rules={[{ required: true, message: 'The recurrent event time range is required!' }]}
                                     >
-                                        <DatePicker disabled={!enabledForm} format={'DD/MM/YYYY HH:00'} showTime />
-                                    </Form.Item >
-                                    :
+                                        <RangePicker format={"HH:00"} />
+                                    </Form.Item>
+
                                     <Form.Item
-                                        label="Time to Due Date"
-                                    >
+                                        label="Week Days"
+                                        name="weekDay"
+                                        rules={[{ required: true, message: 'At least one day must be selected for the event to happen!' }]}>
+                                        <Checkbox.Group
+                                            options={[
+                                                { label: 'Monday', value: 'monday' },
+                                                { label: 'Tuesday', value: 'tuesday' },
+                                                { label: 'Wednesday', value: 'wednesday' },
+                                                { label: 'Thursday', value: 'thursday' },
+                                                { label: 'Friday', value: 'friday' },
+                                                { label: 'Saturday', value: 'saturday' },
+                                                { label: 'Sunday', value: 'sunday' }
+                                            ]}
+                                        />
+                                    </Form.Item>
+
+                                    <Form.Item
+                                        label="Recurrency Frequency">
                                         <Input.Group compact>
                                             <Form.Item
-                                                name={['timeFrame']}
-                                                noStyle
-                                                rules={[{ required: true, message: 'Please select the time frame to be used' }]}
-                                            >
-                                                <Select placeholder="Time frame">
-                                                    <Select.Option value="hour">Hour(s)</Select.Option>
-                                                    <Select.Option value="day">Day(s)</Select.Option>
-                                                </Select>
-                                            </Form.Item>
-                                            <Form.Item
-                                                name={['period']}
+                                                name={'recurrentPeriod'}
                                                 noStyle
                                                 rules={[{ required: true, message: 'The period value is required' }]}
                                             >
-                                                <InputNumber style={{ width: '50%' }} placeholder="Period hours/days" />
+                                                <InputNumber style={{ width: '50%' }} placeholder="Period value" />
+                                            </Form.Item>
+                                            <Form.Item
+                                                name={'recurrentTimeFrame'}
+                                                noStyle
+                                                rules={[{ required: true, message: 'Please select the time frame to be used' }]}
+                                            >
+                                                <Select style={{ width: '50%' }} placeholder="Time frame">
+                                                    <Select.Option value="day">Week(s)</Select.Option>
+                                                    <Select.Option value="month">Month(s)</Select.Option>
+                                                    <Select.Option value="quarter">Quarter(s)</Select.Option>
+                                                    <Select.Option value="semester">Semester(s)</Select.Option>
+                                                    <Select.Option value="year">Year(s)</Select.Option>
+                                                </Select>
                                             </Form.Item>
                                         </Input.Group>
-                                    </Form.Item >
-                            }
+                                        {/* 
 
-                            <Tooltip title="Change to the 'Time to due Date' input that allows you to insert the time to the due date instead of the specific date">
-                                <HourglassOutlined
-                                    className="due-date-toogle"
-                                    style={{ right: this.state.showTimeToDueDateInputField ? '20px' : '80px' }}
-                                    onClick={() => this.setState({ showTimeToDueDateInputField: !this.state.showTimeToDueDateInputField })} />
-                            </Tooltip>
-                        </div>
+                                        TODO: Add the specific target date to the recurrent event
+                                        <Radio.Group
+                                            options={[
+                                                { label: 'Daily', value: 'day' },
+                                                { label: 'Weekly', value: 'week' },
+                                                { label: 'Fortnightly', value: 'fortnight' },
+                                                { label: 'Monthly', value: 'monthly' },
+                                                { label: 'Quarterly ', value: 'quarterly' }
+                                            ]}
+                                            value={value3}
+                                            optionType="button"
+                                        /> */}
+                                    </Form.Item>
+
+                                    <span className="message">
+                                        The recurrent events will ignore jobs appointments when setting the event, so there may be some overlaps to be fixed manually
+                                    </span>
+
+                                </React.Fragment>
+                                :
+                                <React.Fragment>
+                                    <Form.Item
+                                        label="Price"
+                                        name="price"
+                                    >
+                                        <InputNumber
+                                            formatter={price => `$ ${price}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                                            parser={price => price.replace(/\$\s?|(,*)/g, '')}
+                                            min={0}
+                                            precision={2}
+                                            disabled={!enabledForm}
+                                        />
+                                    </Form.Item>
+
+                                    <Form.Item
+                                        label="Estimated Hours"
+                                        name="hours"
+                                        rules={[{ required: true, message: 'The ETA is required to allow the scheduler to distribute time' }]}
+                                    >
+                                        <InputNumber
+                                            disabled={!enabledForm}
+                                            min={0} />
+                                    </Form.Item>
+
+                                    <div className="due-date-wrapper">
+                                        {
+                                            !this.state.showTimeToDueDateInputField
+                                                ?
+                                                <Form.Item
+                                                    label="Due Date"
+                                                    name="dueDate"
+                                                    rules={[{ required: true, message: 'The dude date is required' }, { validator: this.validateDueDate }]}
+                                                >
+                                                    <DatePicker disabled={!enabledForm} format={'DD/MM/YYYY HH:00'} showTime />
+                                                </Form.Item >
+                                                :
+                                                <Form.Item
+                                                    label="Time to Due Date"
+                                                >
+                                                    <Input.Group compact>
+                                                        <Form.Item
+                                                            name={['timeFrame']}
+                                                            noStyle
+                                                            rules={[{ required: true, message: 'Please select the time frame to be used' }]}
+                                                        >
+                                                            <Select placeholder="Time frame">
+                                                                <Select.Option value="hour">Hour(s)</Select.Option>
+                                                                <Select.Option value="day">Day(s)</Select.Option>
+                                                            </Select>
+                                                        </Form.Item>
+                                                        <Form.Item
+                                                            name={['period']}
+                                                            noStyle
+                                                            rules={[{ required: true, message: 'The period value is required' }]}
+                                                        >
+                                                            <InputNumber style={{ width: '50%' }} placeholder="Period hours/days" />
+                                                        </Form.Item>
+                                                    </Input.Group>
+                                                </Form.Item >
+                                        }
+
+                                        <Tooltip title="Change to the 'Time to due Date' input that allows you to insert the time to the due date instead of the specific date">
+                                            <HourglassOutlined
+                                                className="due-date-toogle"
+                                                style={{ right: this.state.showTimeToDueDateInputField ? '20px' : '80px' }}
+                                                onClick={() => this.setState({ showTimeToDueDateInputField: !this.state.showTimeToDueDateInputField })} />
+                                        </Tooltip>
+                                    </div>
 
 
-                        <Tooltip title="Whether the Scheduler should focus on finding continuous periods or just distribute the event in the open periods avaliable">
-                            <Form.Item
-                                label="Continuous Priority"
-                                valuePropName='checked'
-                                name="continuousPeriod">
-                                <Checkbox disabled={!enabledForm}></Checkbox>
-                            </Form.Item>
-                        </Tooltip>
-
+                                    <Form.Item
+                                        label="Continuous Priority"
+                                        valuePropName='checked'
+                                        name="continuousPeriod">
+                                        <Tooltip title="Whether the Scheduler should focus on finding continuous periods or just distribute the event in the open periods avaliable">
+                                            <Checkbox disabled={!enabledForm}></Checkbox>
+                                        </Tooltip>
+                                    </Form.Item>
+                                </React.Fragment>
+                        }
 
                         <span className="message">{this.state.appointmentPreview}</span>
                         {
